144d8c0101fab32f8a198028ebda0f5275cdc845
Madhavam Shahi <madhavampshahi@gmail.com>
2022-09-19 16:25:17 -0500

    added solution of party outlier from my codewars profile

---
+++ party_outlier.dart
@@ -0,0 +1,38 @@
+/*
+Question (Codewars)
+
+You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.
+
+Examples
+[2, 4, 0, 100, 4, 11, 2602, 36]
+Should return: 11 (the only odd number)
+
+[160, 3, 1719, 19, 11, 13, -21]
+Should return: 160 (the only even number)
+
+
+*/
+
+
+int find(List integers) {
+  
+  int odd=0;
+  int even=0;
+  int oddIndex;
+  int evenIndex;
+for(int i=0;i<integers.length;i++){
+    if (integers[i]%2==0)
+{
+      even++;
+     evenIndex=integers[i];
+    }else{
+odd++;
+oddIndex=integers[i];
+}
+  
+  }
+  if(even>=integers.length-1){
+    return oddIndex;
+  }else if(odd>=integers.length-1){return evenIndex;}
+
+}
